548f754ab6ba6b3c9252f366d3bb140bf3b2e322
trhiep <hieptran.pa@gmail.com>
2024-06-06 15:57:53 +0700

    Generate orders razor pages

---
+++ SE1728_Group2_A2/Pages/OrdersManagement/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SE1728_Group2_A2.Models;
+
+namespace SE1728_Group2_A2.Pages.OrdersManagement
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SE1728_Group2_A2.Models.MyStoreContext _context;
+
+        public IndexModel(SE1728_Group2_A2.Models.MyStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Order> Order { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Orders != null)
+            {
+                Order = await _context.Orders
+                .Include(o => o.Staff).ToListAsync();
+            }
+        }
+    }
+}
